e4f730b1ec2b29e07d6eb5df6eccf2b131727dc9
ritu748888 <riturupapara333@gmail.com>
2026-01-23 12:27:07 +0530

    commit for change view

--- python/pollapp/djangotutorial/polls/views.py
+++ python/pollapp/djangotutorial/polls/views.py
@@ -1,11 +1,12 @@
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+from django.http import HttpResponse
+
+from .models import Question
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+# Leave the rest of the views (detail, results, vote) unchanged
