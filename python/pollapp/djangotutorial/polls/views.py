818e90bba5b00740001c86b38648e2d941fa5af9
ritu748888 <riturupapara11@gmail.com>
2026-01-23 12:10:52 +0530

    view updated

--- python/pollapp/djangotutorial/polls/views.py
+++ python/pollapp/djangotutorial/polls/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
 
 
-def index(request):
-    return render(request,'hello.html')
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
