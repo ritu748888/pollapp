a395762e6bf6fc12cf1217722256bd39f5926b83
ritu748888 <ritu748888>
2026-01-30 11:27:14 +0530

    add style.css file

--- python/pollapp/djangotutorial/polls/views.py
+++ python/pollapp/djangotutorial/polls/views.py
@@ -1,13 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.http import HttpResponse
 
-from .models import Question
-
-
-# ...
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+def index(request):
+    return HttpResponse("Hello, welcome to Polls App!")
