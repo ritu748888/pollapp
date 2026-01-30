88b146ae6420907879cbc8ee309a9bc7a7eb806b
ritu748888 <ritu748888>
2026-01-30 11:51:30 +0530

    last changes for admin

--- python/pollapp/djangotutorial/polls/admin.py
+++ python/pollapp/djangotutorial/polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
 from .models import Question
 
-admin.site.register(Question)
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
